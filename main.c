8149037a8694db3b76323e904a6e4ef8b9b8c2a2
Tayane Souza <tayane310@hotmail.com>
2025-11-24 15:09:58 -0300

    Update main.c
    Menu Add

--- main.c
+++ main.c
@@ -16,36 +16,86 @@
 int main(){
     Jogo jogo;
     int gameOver = 1;
+    int Menu = 1; // 1 -> menu, 0 -> jogo rodando
+    int menuSelection = 0; // 0: Start, 1: Instructions, 2: Exit
 
     //Cria a janela;
     InitWindow(LARGURA, ALTURA, "Snake Game");
     SetTargetFPS(60);
     srand(time(NULL));
-    
+
+    // Inicializa uma vez; quando o jogador começar, chama IniciaJogo novamente
     IniciaJogo(&jogo);
+
     while (!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(BLACK);
-        if (gameOver){
-            DesenhaJogo(&jogo);
-            AtualizaRodada(&jogo);
-            ColisaoFood(&jogo); 
-            if(Colisaocobra(&jogo) == 1){
-                gameOver = 0;
+
+        if (Menu){
+            // Navegação do menu
+            if (IsKeyPressed(KEY_DOWN)){
+                menuSelection = (menuSelection + 1) % 3;
             }
-            if(ColisaoBorda(&jogo) == 1){
-                gameOver = 0;
+            if (IsKeyPressed(KEY_UP)){
+                menuSelection = (menuSelection + 2) % 3; // -1 mod 3
             }
-        } else {
-            DrawText("Game Over", 195, 300, 40, WHITE);
+
+            // Desenha o menu
+            DrawText("SNAKE GAME", 200, 80, 40, GREEN);
+            DrawText(menuSelection == 0 ? "Novo Jogo" : "  Novo Jogo", 220, 200, 30, WHITE);
+            DrawText(menuSelection == 1 ? "Ranking" : "  Ranking", 220, 260, 30, WHITE);
+            DrawText(menuSelection == 2 ? "Sair" : "  Sair", 220, 320, 30, WHITE);
+
             if (IsKeyPressed(KEY_ENTER)){
-                Desaloca(&jogo);
-                IniciaJogo(&jogo);
-                gameOver = 1;
+                if (menuSelection == 0){
+                    // inicia o jogo e sai do menu
+                    Desaloca(&jogo);
+                    IniciaJogo(&jogo);
+                    gameOver = 1;
+                    Menu = 0;
+                } else if (menuSelection == 1){
+                    // Vai para tela de ranking até o usuário apertar ENTER
+                    while(!WindowShouldClose()){
+                        BeginDrawing();
+                        ClearBackground(BLACK);
+                        DrawText("Ranking", 200, 60, 40, LIGHTGRAY);
+                        // PARTE PARA INSERIR RANKING AQUI
+                        EndDrawing();
+                        if (IsKeyPressed(KEY_ENTER) || WindowShouldClose()) break;
+                    }
+                } else if (menuSelection == 2){
+                    // Sair do jogo
+                    break;
+                }
+            }
+
+        } else {
+            // Jogo em execução
+            if (gameOver){
+                DesenhaJogo(&jogo);
+                AtualizaRodada(&jogo);
+                ColisaoFood(&jogo);
+                if(Colisaocobra(&jogo) == 1){
+                    gameOver = 0;
+                }
+                if(ColisaoBorda(&jogo) == 1){
+                    gameOver = 0;
+                }
+            } else {
+                DrawText("Game Over", 195, 300, 40, WHITE);
+                DrawText("Press ENTER to return to menu", 120, 360, 20, WHITE);
+                if (IsKeyPressed(KEY_ENTER)){
+                    Desaloca(&jogo);
+                    IniciaJogo(&jogo);
+                    gameOver = 1;
+                    Menu = 1; // volta para o menu
+                }
             }
         }
+
         EndDrawing();
     }
+
     Desaloca(&jogo);
     CloseWindow();
     Desaloca(&jogo);
